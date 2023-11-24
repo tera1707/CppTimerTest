90a2f67889c25c3601812cf23bfb18637821c580
tera1707 <Masa1017>
2023-11-24 12:23:05 +0900

    初回登録

---
+++ WaitableTimerTest/WaitableTimerTest.cpp
@@ -0,0 +1,87 @@
+﻿#include <windows.h>
+#include <stdio.h>
+
+// SetWaitableTimerの解説
+// https://www.codeproject.com/articles/49798/wake-the-pc-from-standby-or-hibernation?fid=1556013&df=90&mpp=25&prof=True&sort=Position&view=Normal&spc=Relaxed&fr=26
+// SetWaitableTimerのfResumeフラグをONにすると、スリープ中でもカウントしてくれそう。だが、ハードやwindowsの設定、その他諸々の条件が揃うと、タイマ満了時に
+// resumeしてしまいそう。
+// →ディスプレイONジイベントで、24h経過を見る方がよさそう。
+
+HANDLE hTimer = NULL;
+LARGE_INTEGER liDueTime;
+long long sec = 5LL;//タイマ満了時間(秒)
+
+/// <param name="lpArg">SetWaitableTimerの第四引数で渡すパラメータ</param>
+/// <param name="dwTimerLowValue">時刻が載ってるらしい（使う気なし）</param>
+/// <param name="dwTimerHighValue">時刻が載ってるらしい（使う気なし）</param>
+VOID CALLBACK TimerAPCProc(LPVOID lpArg, DWORD dwTimerLowValue, DWORD dwTimerHighValue)
+{
+    auto param = (int*)lpArg;
+    printf("タイマ満了来ました。%d\r\n", *param);
+}
+
+// 満了時コールバック無し、シグナル待ちするパターン
+// fResume:SetWaitableTimerのタイマ満了時に、スリープをresumeさせるかどうか
+// →これをTRUEにしていると、スリープ/休止中もカウントを継続できる。で、スリープ中に満了したら、ハードやwindowsの設定が許せば、resumeするらしい。(手持ちPCでは起動しなかった)）
+void SetWaitableTimerWithoutCallback(HANDLE hTimer, LARGE_INTEGER liDueTime, BOOL fResume)
+{
+    MessageBox(NULL, L"start", L"", MB_OK);
+    if (!SetWaitableTimer(hTimer, &liDueTime, 0, NULL, NULL, fResume))
+    {
+        printf("SetWaitableTimer 失敗 (%d)\r\n", GetLastError());
+        return;
+    }
+    printf("SetWaitableTimer 成功 (%d)\r\n", GetLastError());
+
+    // タイマー満了待ち
+    if (WaitForSingleObject(hTimer, INFINITE) != WAIT_OBJECT_0)
+    {
+        printf("WaitForSingleObject 失敗 (%d)\r\n", GetLastError());
+    }
+    else
+    {
+        printf("タイマーがシグナル状態になりました\r\n");
+    }
+    MessageBox(NULL, L"stop", L"", MB_OK);
+}
+
+void SetWaitableTimerWithCallback(HANDLE hTimer, LARGE_INTEGER liDueTime, BOOL fResume)
+{
+    MessageBox(NULL, L"start", L"", MB_OK);
+
+    // 満了時コールバック有りパターン
+    int param = 99;
+    if (!SetWaitableTimer(hTimer, &liDueTime, 0, TimerAPCProc, &param, fResume))
+    {
+        printf("SetWaitableTimer 失敗 (%d)\r\n", GetLastError());
+        return;
+    }
+    // 注意！Sleep()で待っていると、ここと同じスレッドで実行されることになるTimerAPCProcが呼べなくなる。
+    // WaitableTimerを使っている場合にスレッドをスリープさせたい場合は、SleepEx()で行う必要がある。
+    // (SleepEx()の第三引数はTRUE=アラート可能である必要がある。)
+    // また、タイマ満了した結果、SleepEx()の待ちは終了する
+    SleepEx(INFINITE, TRUE);
+
+    MessageBox(NULL, L"stop", L"", MB_OK);
+}
+
+int main()
+{
+    auto oneSec = -10000000LL;// 1=100nsec(ナノ秒)なので、これで1秒になる
+    liDueTime.QuadPart = sec * oneSec;
+
+    // タイマ作成
+    hTimer = CreateWaitableTimer(NULL, TRUE, L"aaa");
+    if (NULL == hTimer)
+    {
+        printf("CreateWaitableTimer 失敗 (%d)\r\n", GetLastError());
+        return 1;
+    }
+#if 0
+    SetWaitableTimerWithoutCallback(hTimer, liDueTime, FALSE);
+#else
+    SetWaitableTimerWithCallback(hTimer, liDueTime, FALSE);
+#endif
+
+    return 0;
+}
---
+++ WindowsProject1/WindowsProject1.cpp
@@ -0,0 +1,106 @@
+﻿// WindowsProject1.cpp : アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "framework.h"
+#include "WindowsProject1.h"
+#include <windows.h>
+#include <stdio.h>
+#include <thread>
+
+#define MAX_LOADSTRING 100
+
+// グローバル変数:
+HINSTANCE hInst;                                // 現在のインターフェイス
+WCHAR szTitle[MAX_LOADSTRING];                  // タイトル バーのテキスト
+WCHAR szWindowClass[MAX_LOADSTRING];            // メイン ウィンドウ クラス名
+
+// このコード モジュールに含まれる関数の宣言を転送します:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+//HANDLE hTimer = NULL;
+LARGE_INTEGER liDueTime;
+
+static std::thread th_a;
+
+HANDLE hTimer = NULL;
+HANDLE hTimerQueue = NULL;
+//https://www.keicode.com/windows/win12.php
+VOID CALLBACK TimerRoutine(PVOID lpParam, BOOLEAN TimerOrWaitFired)
+{
+    if (lpParam == NULL)
+    {
+        printf("TimerRoutine lpParam is NULL\n");
+    }
+    else
+    {
+        // lpParam points to the argument; in this case it is an int
+
+        printf("Timer routine called. Parameter is %d.\n",
+            *(int*)lpParam);
+        if (TimerOrWaitFired)
+        {
+            printf("The wait timed out.\n");
+        }
+        else
+        {
+            printf("The wait event was signaled.\n");
+        }
+    }
+}
+void MyTimerStart()
+{
+    hTimerQueue = CreateTimerQueue();
+    if (NULL == hTimerQueue)
+    {
+        printf("CreateTimerQueue failed (%d)\n", GetLastError());
+        return ;
+    }
+
+    // Set a timer to call the timer routine in 10 seconds.
+    if (!CreateTimerQueueTimer(&hTimer, hTimerQueue,
+        (WAITORTIMERCALLBACK)TimerRoutine, NULL, 1000, 0, 0))
+    {
+        printf("CreateTimerQueueTimer failed (%d)\n", GetLastError());
+        return ;
+    }
+
+}
+
+BOOL CALLBACK MyDlgProc(HWND hDlg, UINT msg, WPARAM wp, LPARAM lp)
+{
+    switch (msg) {
+    case WM_COMMAND:
+        switch (LOWORD(wp)) {
+        case IDOK:
+            EndDialog(hDlg, IDOK);
+            return TRUE;
+        case IDCANCEL:
+            EndDialog(hDlg, IDCANCEL);
+            return TRUE;
+        case IDC_BUTTON1:
+            MyTimerStart();
+            break;
+        }
+        return FALSE;
+    }
+    return FALSE;
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    hInst = hInstance;
+
+    // ダイアログの名前と、プロシージャを指定してDialogBoxを開く
+    DialogBox(hInst, L"MYDLG", NULL, (DLGPROC)MyDlgProc);
+
+    return (int)0;
+}
+
+
+
